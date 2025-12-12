ab7834b5526b1c677784acc4a83091346cef0144
Mihir Makwana <makwanamihir02@gmail.com>
2025-12-12 16:12:19 +0530

    somethingnew

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -2,6 +2,13 @@ import { app } from "../src/server";
 import connectDB from "../src/config/database";
 import { ensureAdmin } from "../src/utils/bootstrapAdmin";
 
-connectDB().then(() => ensureAdmin());
+console.log("Vercel API starting...");
+console.log("Vercel: ADMIN_EMAIL present:", !!process.env.ADMIN_EMAIL);
+console.log("Vercel: ADMIN_PASSWORD present:", !!process.env.ADMIN_PASSWORD);
+
+connectDB().then(async () => {
+  console.log("Vercel: DB connected. Calling ensureAdmin...");
+  await ensureAdmin();
+});
 
 export default app;
